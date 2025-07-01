a41508624418d92a35a9a47fe4f0656d00dc4b8f
Niyozbek <imniyozbek@gmail.com>
2025-07-01 14:38:01 +0500

    Token order is switched for visibility.

--- src/main/java/uk/ac/swansea/autogradingwebservice/auth/controllers/dto/LoginResponseDto.java
+++ src/main/java/uk/ac/swansea/autogradingwebservice/auth/controllers/dto/LoginResponseDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponseDto {
+    private String token;
     private String username;
     private String role;
-    private String token;
 }
